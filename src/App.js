fc52fcf81d0c97170dab4a744e172deb0a2a530f
lubomirKavetskiy <lubomirkavetskiy@gmail.com>
2018-04-15 22:59:43 +0300

    I've used simple props and state for components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
+import Drawing from './Person/Drawing';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: `Lubomir`, age: 32},
+      {name: `Yulia`, age: 28},
+      {name: `Oleg`, age: 42},
+    ]
+  }
+
+  switchNameHandler = () => {
+    //console.log(`was clicked`);
+    this.setState({
+      persons: [
+        {name: `Lubomir_2018`, age: 32},
+        {name: `Yulia`, age: 28},
+        {name: `Oleg`, age: 64},
+      ]
+    });
+  }
+  // switchNameHandler(){console.log(`was clicked`);} 
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'am React App</h1>
         <p>This is really working</p>
+        {/* <Person name = "Lubomir" age = "31"/>
+        <Person name = "Yulia" age = "28">I love drawing</Person>
+        <Person name = "Yulia" age = "28"><Drawing todo = "drawing"/></Person>
+        <Person name = "Oleg" age = "45"/> */}
+        <button onClick = {this.switchNameHandler}>Switch name</button>
+        <Person name = {this.state.persons[0].name} age = {this.state.persons[0].age} />
+        <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age}>I love drawing</Person>
+        <Person name = {this.state.persons[1].name} age = {this.state.persons[1].age}><Drawing todo = "drawing"/></Person>
+        <Person name = {this.state.persons[2].name} age = {this.state.persons[2].age} />
       </div>
     );
   }
---
+++ src/Person/Drawing.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const drawing = props => <p>I love {props.todo}</p>;
+
+export default drawing;
